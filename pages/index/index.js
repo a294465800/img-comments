85a658ab331cbbd2cc69b1b2192da87365c1ffdd
SHINING <jack7770126@gmail.com>
2017-09-19 09:47:58 +0800

    [teacher_index] finish this page.

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,9 +10,17 @@ Page({
 
   },
 
+  //学生入口
   goToStudent() {
     wx.navigateTo({
       url: '/pages/student_index/student_index',
+    })
+  },
+
+  //教师入口
+  gotToTeacher() {
+    wx.navigateTo({
+      url: '/pages/teacher_index/teacher_index',
     })
   }
 })
---
+++ pages/teacher_index/teacher_index.js
@@ -0,0 +1,16 @@
+// pages/teacher_index/teacher_index.js
+Page({
+
+  data: {
+  
+  },
+
+  onLoad(options) {
+  
+  },
+
+  teacherLogin(e){
+    const password = e.detail.value.password
+    console.log(e)
+  }
+})
